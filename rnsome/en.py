e69513f64a1903649a0efca385d7a11bc8d0d794
Aj S <105689704+ajdev05@users.noreply.github.com>
2022-09-02 08:45:14 -0400

    Update en.py

--- rnsome/en.py
+++ rnsome/en.py
@@ -1,7 +1,7 @@
 #!usr/bin/env python3
 
 ######################################################
-# Copy Right to ~> Security On PePs & Team           #
+# Copy Right to ~> Ajdev05.                          #
 # Follow Me on Github ~> https://github.com/ajdev05/ #
 # Discord ~> https://discord.gg/KBSQyjWVzG           #
 # *Only for testing and educational Purposes*        #
